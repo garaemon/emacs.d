035b62bdd2049059ca83627383847dde211ace35
garaemon <garaemon@gmail.com>
2013-10-12 18:05:13 +0900

    hotfix

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -366,7 +366,7 @@
              (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
              (local-set-key "\C-cb" 'js-send-buffer)
              (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
-             (local-set-key "\C-cl" 'js-load-file-and-go)
+             (local-set-key "\C-c\C-l" 'js-load-file-and-go)
              ))
 (defun js-other-window ()
   "Run JavaScript on other window"
