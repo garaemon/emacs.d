8010072ddcd6386cf06675a39e91d1b90aa4ce81
garaemon <garaemon@gmail.com>
2014-02-27 15:30:06 +0900

    not use find-file for C-x C-f

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -1178,7 +1178,7 @@
 (add-to-list 'helm-completing-read-handlers-alist '(find-file . nil))
 
 (define-key global-map (kbd "M-x")     'helm-M-x)
-(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+;;(define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (define-key global-map (kbd "C-x C-r") 'helm-recentf)
 (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
 (define-key global-map (kbd "C-c i")   'helm-imenu)
