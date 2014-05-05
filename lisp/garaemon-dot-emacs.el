c2ececa8fac97e6d6f4a0f1f89bf8c1c8731fd75
garaemon <garaemon@gmail.com>
2014-05-05 12:52:24 +0900

    update

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -35,35 +35,35 @@
 
 (global-set-key "\C-xp" (lambda () (interactive) (other-window -1)))
 
-(when (require 'auto-complete nil t)
-  (require 'auto-complete-config)
-  (global-auto-complete-mode t)
-  (define-key ac-complete-mode-map "\t" 'ac-expand)
-  (define-key ac-complete-mode-map "\r" 'ac-complete)
-  (define-key ac-complete-mode-map "\M-n" 'ac-next)
-  (define-key ac-complete-mode-map "\M-p" 'ac-previous)
-  (setq ac-auto-start 2)
-  (setq ac-dwim t)
-  (set-default 'ac-sources '(ac-source-yasnippet
-                             ac-source-abbrev
-                             ac-source-words-in-buffer))
-  (setq ac-modes
-    (append ac-modes
-        '(emacs-lisp-mode
-          lisp-mode
-          euslisp-mode
-          html-mode
-          cmake-mode 
-          xhtml-mode
-          ;;org-mode
-          )))
-  (add-hook 'emacs-lisp-mode-hook
-        (lambda ()
-          (setq ac-sources '(ac-source-yasnippet
-                 ac-source-abbrev
-                 ac-source-words-in-buffer
-                 ac-source-symbols))))
-  )
+;; (when (require 'auto-complete nil t)
+;;   (require 'auto-complete-config)
+;;   (global-auto-complete-mode t)
+;;   (define-key ac-complete-mode-map "\t" 'ac-expand)
+;;   (define-key ac-complete-mode-map "\r" 'ac-complete)
+;;   (define-key ac-complete-mode-map "\M-n" 'ac-next)
+;;   (define-key ac-complete-mode-map "\M-p" 'ac-previous)
+;;   (setq ac-auto-start 2)
+;;   (setq ac-dwim t)
+;;   (set-default 'ac-sources '(ac-source-yasnippet
+;;                              ac-source-abbrev
+;;                              ac-source-words-in-buffer))
+;;   (setq ac-modes
+;;     (append ac-modes
+;;         '(emacs-lisp-mode
+;;           lisp-mode
+;;           euslisp-mode
+;;           html-mode
+;;           cmake-mode 
+;;           xhtml-mode
+;;           ;;org-mode
+;;           )))
+;;   (add-hook 'emacs-lisp-mode-hook
+;;         (lambda ()
+;;           (setq ac-sources '(ac-source-yasnippet
+;;                  ac-source-abbrev
+;;                  ac-source-words-in-buffer
+;;                  ac-source-symbols))))
+;;   )
 
 (global-set-key "\C-h" 'backward-delete-char)
 (global-set-key "\M-h" 'help-for-help)
@@ -118,6 +118,8 @@
                 markdown-mode-hook
                 python-mode-hook
                 lisp-mode-hook euslisp-mode-hook
+                cmake-mode-hook
+                javascript-mode-hook js-mode-hook
                 emacs-lisp-mode-hook))
   (add-hook mode (lambda () (interactive) (column-marker-2 80)))
   )
@@ -344,19 +346,19 @@
     (local-set-key [(control .)] 'flyspell-auto-correct-word)))
 
 ;; flycheck
-(require 'flycheck)
-(defun flycheck-exclude-tramp ()
-  (unless (or (and (fboundp 'tramp-tramp-file-p)
-                   (tramp-tramp-file-p buffer-file-name))
-              (string-match "sudo:.*:" (buffer-file-name)))
-    (flycheck-mode t)
-  ))
+;; (require 'flycheck)
+;; (defun flycheck-exclude-tramp ()
+;;   (unless (or (and (fboundp 'tramp-tramp-file-p)
+;;                    (tramp-tramp-file-p buffer-file-name))
+;;               (string-match "sudo:.*:" (buffer-file-name)))
+;;     (flycheck-mode t)
+;;   ))
 
-(add-hook 'python-mode-hook 'flycheck-exclude-tramp)
-(add-hook 'emacs-lisp-mode-hook 'flycheck-exclude-tramp)
-(add-hook 'c-mode-hook 'flycheck-exclude-tramp)
-(add-hook 'c++-mode-hook 'flycheck-exclude-tramp)
-(add-hook 'sh-mode-hook 'flycheck-exclude-tramp)
+;; (add-hook 'python-mode-hook 'flycheck-exclude-tramp)
+;; (add-hook 'emacs-lisp-mode-hook 'flycheck-exclude-tramp)
+;; (add-hook 'c-mode-hook 'flycheck-exclude-tramp)
+;; (add-hook 'c++-mode-hook 'flycheck-exclude-tramp)
+;; (add-hook 'sh-mode-hook 'flycheck-exclude-tramp)
 
 
 ;; bind to M-h
