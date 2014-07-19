0dfa20f983abfcc7abd782193f6add46e640503f
Ryohei Ueda <garaemon@gmail.com>
2014-07-19 16:14:54 +0900

    add new snippets and helm-c-yasnippet

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -1416,6 +1416,17 @@ static char * arrow_right[] = {
 (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
 ;; 既存スニペットを閲覧・編集する
 (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+(setq yas-buffer-local-condition
+      '(or (not (or (string= "font-lock-comment-face"
+                             (get-char-property (point) 'face))
+                    (string= "font-lock-string-face"
+                             (get-char-property (point) 'face))))
+           '(require-snippet-condition . force-in-comment)))
+
+(when (require 'helm-c-yasnippet nil t)
+  (setq helm-c-yas-space-match-any-greedy t) ;[default: nil]
+  (global-set-key (kbd "M--") 'helm-c-yas-complete)
+  )
 
 
 (provide 'garaemon-dot-emacs)
