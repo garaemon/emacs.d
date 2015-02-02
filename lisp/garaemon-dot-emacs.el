e67cdc5c55df8b5f59de488a67278853c02cfc02
Ryohei Ueda <garaemon@gmail.com>
2015-02-03 03:52:45 +0900

    Update

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -1574,4 +1574,30 @@ downcased, no preceding underscore.
 (symon-mode)
 
 (global-linum-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(eshell-ask-to-save-history (quote always))
+ '(eshell-history-size 1000)
+ '(eshell-ls-dired-initial-args (quote ("-h")))
+ '(eshell-ls-exclude-regexp "~\\'")
+ '(eshell-ls-use-in-dired t nil (em-ls))
+ '(eshell-modules-list (quote (eshell-alias eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-ls eshell-pred eshell-prompt eshell-rebind eshell-script eshell-smart eshell-term eshell-unix eshell-xtra)))
+ '(eshell-prefer-o-shell t nil (eshell))
+ '(eshell-stringify-t nil)
+ '(eshell-term-name "ansi")
+ '(eshell-visual-commands (quote ("vi" "top" "screen" "less" "lynx" "ssh" "rlogin" "telnet")))
+ '(wakatime-cli-path "~/gprog/wakatime/wakatime-cli.py")
+ '(yas-trigger-key "TAB"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#7f8c8d")))))
+
+
 (provide 'garaemon-dot-emacs)
