50d7da09f93dc35d69c937e22148c1ea05a6da01
Ryohei Ueda <garaemon@gmail.com>
2015-02-13 03:57:27 +0900

    Enable auto-complete again

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -35,35 +35,35 @@
 
 (global-set-key "\C-xp" (lambda () (interactive) (other-window -1)))
 
-;; (when (require 'auto-complete nil t)
-;;   (require 'auto-complete-config)
-;;   (global-auto-complete-mode t)
-;;   (define-key ac-complete-mode-map "\t" 'ac-expand)
-;;   (define-key ac-complete-mode-map "\r" 'ac-complete)
-;;   (define-key ac-complete-mode-map "\M-n" 'ac-next)
-;;   (define-key ac-complete-mode-map "\M-p" 'ac-previous)
-;;   (setq ac-auto-start 2)
-;;   (setq ac-dwim t)
-;;   (set-default 'ac-sources '(ac-source-yasnippet
-;;                              ac-source-abbrev
-;;                              ac-source-words-in-buffer))
-;;   (setq ac-modes
-;;     (append ac-modes
-;;         '(emacs-lisp-mode
-;;           lisp-mode
-;;           euslisp-mode
-;;           html-mode
-;;           cmake-mode
-;;           xhtml-mode
-;;           ;;org-mode
-;;           )))
-;;   (add-hook 'emacs-lisp-mode-hook
-;;         (lambda ()
-;;           (setq ac-sources '(ac-source-yasnippet
-;;                  ac-source-abbrev
-;;                  ac-source-words-in-buffer
-;;                  ac-source-symbols))))
-;;   )
+(when (require 'auto-complete nil t)
+  (require 'auto-complete-config)
+  (global-auto-complete-mode t)
+  (define-key ac-complete-mode-map "\t" 'ac-expand)
+  (define-key ac-complete-mode-map "\r" 'ac-complete)
+  (define-key ac-complete-mode-map "\M-n" 'ac-next)
+  (define-key ac-complete-mode-map "\M-p" 'ac-previous)
+  (setq ac-auto-start 2)
+  (setq ac-dwim t)
+  (set-default 'ac-sources '(ac-source-yasnippet
+                             ac-source-abbrev
+                             ac-source-words-in-buffer))
+  (setq ac-modes
+    (append ac-modes
+        '(emacs-lisp-mode
+          lisp-mode
+          euslisp-mode
+          html-mode
+          cmake-mode
+          xhtml-mode
+          ;;org-mode
+          )))
+  (add-hook 'emacs-lisp-mode-hook
+        (lambda ()
+          (setq ac-sources '(ac-source-yasnippet
+                 ac-source-abbrev
+                 ac-source-words-in-buffer
+                 ac-source-symbols))))
+  )
 
 (global-set-key "\C-h" 'backward-delete-char)
 (global-set-key "\M-h" 'help-for-help)
