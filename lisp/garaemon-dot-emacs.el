4fc05b6f46cdd250d86bb09af61b44a77f764d27
Ryohei Ueda <garaemon@gmail.com>
2015-08-21 03:13:33 +0900

    3-way split

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -1303,5 +1303,29 @@ downcased, no preceding underscore.
 (add-hook 'c++-mode-hook 'c-turn-on-eldoc-mode)
 (setq c-eldoc-buffer-regenerate-time 60)
 
+(defun split-window-vertically-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-vertically)
+    (progn
+      (split-window-vertically
+       (- (window-height) (/ (window-height) num_wins)))
+      (split-window-vertically-n (- num_wins 1)))))
+(defun split-window-horizontally-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-horizontally)
+    (progn
+      (split-window-horizontally
+       (- (window-width) (/ (window-width) num_wins)))
+      (split-window-horizontally-n (- num_wins 1)))))
+(global-set-key "\C-x@" '(lambda ()
+                           (interactive)
+                           (split-window-vertically-n 3)))
+(global-set-key "\C-x#" '(lambda ()
+                           (interactive)
+                           (split-window-horizontally-n 3)))
+
+
 (require 'coffee-mode)
 (provide 'garaemon-dot-emacs)
