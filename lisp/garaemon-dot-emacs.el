e176742b82b1e905b9221f06101132d8b71c840b
Ryohei Ueda <garaemon@gmail.com>
2017-05-03 01:12:35 +0900

    Setup web-mode

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -1796,5 +1796,12 @@ With prefix ARG non-nil, insert the result at the end of region."
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook 'web-mode-hook)
+
 
 (provide 'garaemon-dot-emacs)
