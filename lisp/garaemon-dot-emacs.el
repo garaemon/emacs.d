76af5a2c8716f65c24229042554c3431a54304ac
Ryohei Ueda <garaemon@gmail.com>
2018-04-21 13:29:05 +0900

    Specify indent for <style> tag

--- lisp/garaemon-dot-emacs.el
+++ lisp/garaemon-dot-emacs.el
@@ -434,6 +434,7 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-script-padding 2)
+  (setq web-mode-style-padding 2)
   (setq tab-width 2)
   )
 (add-hook 'web-mode-hook 'my-web-mode-hook)
